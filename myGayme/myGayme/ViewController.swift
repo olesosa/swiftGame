b9b357c5e74b4bd36bac6e3324854ba446213520
Олесь <102287993+olesosa@users.noreply.github.com>
2023-10-20 11:37:10 +0300

    Add files via upload

---
+++ myGayme/myGayme/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  myGayme
+//
+//  Created by Admin on 13.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var topCardViews: [UIView]!
+    @IBOutlet var bottomCardViews: [UIView]!
+    @IBOutlet var btnChangeCard: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func flipCard(_ sender: Any) {
+    }
+    
+}
+
